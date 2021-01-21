af87d2a99df486389916dfa4e899a4e06a4b0cb7
Lucas Bomfim <lucasliko@hotmail.com>
2021-01-21 11:35:32 -0300

    Primeira versão criada

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -4,62 +4,67 @@ import React, { Component  } from "react";
 import Buttons from './Buttons'
 import Display from './Display'
 
+
 class Calculadora extends Component {
+
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       display: '0',
-      valores: [0, 0],
-      current: 0,
-      operation: null,
+      operations: []
     }
 
     this.limpar = this.limpar.bind(this)
-    this.addOperador = this.addOperador.bind(this)
+    this.resultado = this.resultado.bind(this)
     this.addDigito = this.addDigito.bind(this)
   }
   
 
   limpar() {
+    this.setState(state => ({display: '0', operations: []}))
+  }
+
+  resultado() {
+    let conta = this.state.display.split(/[*\-+/]/ig)
+    let novaConta = []
+
+     conta.forEach((element, indice) => {
+      novaConta.push(Number(element))
+      
+      if (indice < this.state.operations.length) {
+        novaConta.push(this.state.operations[indice])
+      }
+    })
+
+    let resultado = 0;
+
+    for (let i = 0; i < novaConta.length; i++) {
+      if (novaConta[i] === '+') {
+        resultado = resultado === 0 ? novaConta[i - 1] + novaConta[i + 1] : resultado + novaConta[i + 1]
+      } else if (novaConta[i] === '-') {
+         resultado = resultado === 0 ? novaConta[i - 1] - novaConta[i + 1] : resultado - novaConta[i + 1]
+      } else if (novaConta[i] === '/') {
+         resultado = resultado === 0 ? novaConta[i - 1] / novaConta[i + 1] : resultado / novaConta[i + 1]
+      } else if (novaConta[i] === '*') {
+         resultado = resultado === 0 ? novaConta[i - 1] * novaConta[i + 1] : resultado * novaConta[i + 1]
+      }
+    }
+
     this.setState(state => ({
-      display: '0',
-      valores: [0, 0],
-      operation: null
+      display: resultado,
+      operations: []
     }))
   }
 
-  addOperador(operacao) {
-   switch(operacao) {
-     case "+":
-     break;
-     case "-":
-     console.log(operacao)
-     break;
-     case "*":
-     console.log(operacao)
-     break;
-     case "/":
-     console.log(operacao)
-     break;
-     case "=":
-     console.log(operacao)
-     break;
-     default:
-     console.log(operacao)
-     break;
-     
-   }
-  }
-
   addDigito(n) {
-    if (n === '.' && this.state.inicial.includes('.')) {
-      return
-    } else {
-      this.setState(state => ({
-        display: state.display === '0' ? state.display = n : state.display += n
-      }))
-    }
+    let novoArray = [...this.state.operations]
+    if (isNaN(Number(n)) && n !== '.') novoArray.push(n)
+
+    this.setState(state => ({
+      display: state.display === '0' ? state.display = n : state.display += n,
+      operations: novoArray
+    }))
   }
 
   render() {
@@ -69,15 +74,15 @@ class Calculadora extends Component {
       <Display operacao={this.state.display}/>
     </div>
     <div className="calculadora">
-      <Buttons value="/" type="symbol" click={this.addOperador}/>
-      <Buttons value="*" type="symbol" click={this.addOperador} />
-      <Buttons value="-" type="symbol" click={this.addOperador} />
-      <Buttons value="+" type="symbol" click={this.addOperador} />
+      <Buttons value="/" type="symbol" click={this.addDigito}/>
+      <Buttons value="*" type="symbol" click={this.addDigito} />
+      <Buttons value="-" type="symbol" click={this.addDigito} />
+      <Buttons value="+" type="symbol" click={this.addDigito} />
 
       <Buttons value="7" type="number" click={this.addDigito} />
       <Buttons value="8" type="number" click={this.addDigito}/>
       <Buttons value="9" type="number" click={this.addDigito}/>
-      <Buttons value="=" type="result" click={this.addOperador}/>
+      <Buttons value="=" type="result" click={this.resultado}/>
 
       <Buttons value="4" type="number" click={this.addDigito}/>
       <Buttons value="5" type="number" click={this.addDigito}/>
